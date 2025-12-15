3d7c2178d7ae30da4bd3c92493c2b62a96309842
Cristian Díaz <xtndiaz@gmail.com>
2025-12-15 18:39:10 +0200

    add polygon collision for rotated rectangles and more

--- src/components/collider-components.ts
+++ src/components/collider-components.ts
@@ -1,49 +1,48 @@
-import { Point, type PointData } from 'pixi.js'
-import { Component, distanceSquared, testForAABBV } from '../core'
+import { Graphics, Point, type PointData } from 'pixi.js'
+import { Component, distanceSquared, testForAABBV, Vector } from '../core'
 
 export abstract class CollisionShape {
+  readonly position = new Point()
   readonly vertices: number[]
   readonly aabb: number[] = Array(4).fill(0)
-  readonly center = new Point()
 
-  constructor(
-    public x: number,
-    public y: number,
-    protected points: number[],
-  ) {
-    this.vertices = [...points]
-
-    this.update({ x: 0, y: 0 }, 0)
+  protected _rotation = 0
+  protected isRotated = false
+  set rotation(val: number) {
+    this._rotation = val
+    this.isRotated = val != 0
   }
 
-  update(wPos: PointData, rot: number): void {
+  constructor(protected readonly points: number[]) {
+    this.vertices = [...points]
+
+    this.update()
+  }
+
+  update(): void {
     let minX = Infinity
     let maxX = -Infinity
     let minY = Infinity
     let maxY = -Infinity
 
-    for (let i = 0; i < this.points.length; i++) {
-      const cos = Math.cos(rot)
-      const sin = Math.sin(rot)
+    for (let i = 0; i < this.points.length; i += 2) {
+      const cos = Math.cos(this._rotation)
+      const sin = Math.sin(this._rotation)
+      const x = this.points[i]!
+      const y = this.points[i + 1]!
 
-      if (i % 2 == 0) {
-        const x = this.points[i]! + this.x
-        const y = this.points[i + 1]! + this.y
-        this.vertices[i] = x * cos - y * sin + wPos.x
-        minX = Math.min(minX, this.vertices[i]!)
-        maxX = Math.max(maxX, this.vertices[i]!)
-      } else {
-        const x = this.points[i - 1]! + this.x
-        const y = this.points[i]! + this.y
-        this.vertices[i] = x * sin + y * cos + wPos.y
-        minY = Math.min(minY, this.vertices[i]!)
-        maxY = Math.max(maxY, this.vertices[i]!)
-      }
+      // Rotation matrix: https://en.wikipedia.org/wiki/Rotation_matrix
+      this.vertices[i] = x * cos - y * sin + this.position.x
+      this.vertices[i + 1] = x * sin + y * cos + this.position.y
+      minX = Math.min(minX, this.vertices[i]!)
+      maxX = Math.max(maxX, this.vertices[i]!)
+      minY = Math.min(minY, this.vertices[i + 1]!)
+      maxY = Math.max(maxY, this.vertices[i + 1]!)
+
       this.aabb[0] = minX
       this.aabb[1] = minY
       this.aabb[2] = maxX
       this.aabb[3] = maxY
-      // console.log(this.aabb)
     }
   }
 
@@ -59,24 +58,64 @@ export abstract class CollisionShape {
   }
   abstract collidesRectangle(rectangle: RectangleCS): boolean
   abstract collidesCircle(circle: CircleCS): boolean
+
+  /* 
+    Following SAT – Separating Axis Theorem: https://www.sevenson.com.au/programming/sat/ 
+  */
+  collidesPolygon(polygon: PolygonCS): boolean {
+    function overlapsProjection(from: number[], to: number[]) {
+      let dot: number, minDotFrom: number, maxDotFrom: number, minDotTo: number, maxDotTo: number
+      for (let i = 0; i < from.length; i += 2) {
+        const v0 = [from[i]!, from[i + 1]!]
+        const v1 = [from[(i + 2) % from.length]!, from[(i + 3) % from.length]!]
+        const normal = new Vector(v1[1]! - v0[1]!, v0[0]! - v1[0]!)
+        minDotFrom = Infinity
+        maxDotFrom = -Infinity
+        for (let j = 0; j < from.length; j += 2) {
+          dot = normal.dot({ x: from[j]!, y: from[j + 1]! })
+          minDotFrom = Math.min(minDotFrom, dot)
+          maxDotFrom = Math.max(maxDotFrom, dot)
+        }
+        minDotTo = Infinity
+        maxDotTo = -Infinity
+        for (let j = 0; j < to.length; j += 2) {
+          dot = normal.dot({ x: to[j]!, y: to[j + 1]! })
+          minDotTo = Math.min(minDotTo, dot)
+          maxDotTo = Math.max(maxDotTo, dot)
+        }
+        if (minDotFrom > maxDotTo || minDotTo > maxDotFrom) {
+          return false
+        }
+      }
+      return true
+    }
+    return (
+      overlapsProjection(this.vertices, polygon.vertices) &&
+      overlapsProjection(polygon.vertices, this.vertices)
+    )
+  }
+
+  createDebugGraphics(): Graphics {
+    return new Graphics().poly(this.points).stroke({ width: 1, color: 0x00ffff })
+  }
 }
 
 export class RectangleCS extends CollisionShape {
   constructor(
-    x: number,
-    y: number,
+    private x: number,
+    private y: number,
     public w: number,
     public h: number,
   ) {
-    super(x, y, [0, 0, w, 0, w, h, 0, h])
+    super([x, y, x + w, y, x + w, y + h, x, y + h])
   }
 
-  update(wPos: PointData, rot: number): void {
-    if (rot != 0) {
-      super.update(wPos, rot)
+  update(): void {
+    if (this._rotation != 0) {
+      super.update()
     } else {
-      this.aabb[0] = this.x + wPos.x
-      this.aabb[1] = this.y + wPos.y
+      this.aabb[0] = this.x + this.position.x
+      this.aabb[1] = this.y + this.position.y
       this.aabb[2] = this.aabb[0] + this.w
       this.aabb[3] = this.aabb[1] + this.h
 
@@ -88,13 +127,15 @@ export class RectangleCS extends CollisionShape {
       this.vertices[5] = this.aabb[3]
       this.vertices[6] = this.aabb[0]
       this.vertices[7] = this.aabb[3]
-
-      this.center.set(this.aabb[0] + this.w * 0.5, this.aabb[1] + this.h * 0.5)
     }
   }
 
   collidesRectangle(rectangle: RectangleCS): boolean {
-    return testForAABBV(this.aabb, rectangle.aabb)
+    if (this.isRotated || rectangle.isRotated) {
+      return this.collidesPolygon(rectangle as PolygonCS)
+    } else {
+      return testForAABBV(this.aabb, rectangle.aabb)
+    }
   }
   collidesCircle(circle: CircleCS): boolean {
     return testForAABBV(this.aabb, circle.aabb) // TODO
@@ -103,27 +144,44 @@ export class RectangleCS extends CollisionShape {
 
 export class CircleCS extends CollisionShape {
   constructor(
-    x: number,
-    y: number,
+    private x: number,
+    private y: number,
     public r: number,
   ) {
-    super(x, y, [])
+    super([])
   }
 
-  update(wPos: PointData, _: number): void {
-    this.aabb[0] = this.x + wPos.x - this.r
-    this.aabb[1] = this.y + wPos.y - this.r
+  update(): void {
+    this.aabb[0] = this.x + this.position.x - this.r
+    this.aabb[1] = this.y + this.position.y - this.r
     this.aabb[2] = this.aabb[0] + 2 * this.r
     this.aabb[3] = this.aabb[1] + 2 * this.r
-
-    this.center.set(this.x + wPos.x, this.y + wPos.y)
   }
 
   collidesRectangle(rectangle: RectangleCS): boolean {
     return testForAABBV(this.aabb, rectangle.aabb)
   }
   collidesCircle(circle: CircleCS): boolean {
-    return distanceSquared(this.center, circle.center) < Math.pow(this.r + circle.r, 2)
+    return distanceSquared(this.position, circle.position) < Math.pow(this.r + circle.r, 2)
+  }
+  collidesPolygon(polygon: PolygonCS): boolean {
+    return false
+  }
+
+  createDebugGraphics(): Graphics {
+    return new Graphics().circle(this.x, this.y, this.r).stroke({ width: 1, color: 0x00ffff })
+  }
+}
+
+export class PolygonCS extends CollisionShape {
+  collidesCircle(circle: CircleCS): boolean {
+    return false
+  }
+  collidesRectangle(rectangle: RectangleCS): boolean {
+    return false
+  }
+  collidesPolygon(polygon: PolygonCS): boolean {
+    return true
   }
 }
 
@@ -144,7 +202,10 @@ export class Collider<Shape extends CollisionShape> extends Component {
   }
 
   update(worldPos: PointData, rotation: number): void {
-    this.shape.update(worldPos, rotation)
+    this.shape.position.copyFrom(worldPos)
+    this.shape.rotation = rotation
+
+    this.shape.update()
   }
 
   collides<T extends CollisionShape>(other: Collider<T>): boolean {
--- src/systems/CollisionSystem.ts
+++ src/systems/CollisionSystem.ts
@@ -1,4 +1,4 @@
-import { System, testForAABBV, type SignalBus, type World } from '../core'
+import { System, type SignalBus, type World } from '../core'
 import { CircleCS, Collider } from '../components'
 import { CollisionSignal } from '../signals'
 import type { HUD } from '../ui'
@@ -6,38 +6,28 @@ import { Graphics } from 'pixi.js'
 
 export class CollisionSystem extends System {
   init(world: World, hud: HUD, sb: SignalBus): void {
-    // const ecs = world.getEntitiesWithComponent(Collider)
-    // for (const { e, c } of ecs) {
-    //   if (c.shape instanceof CircleCS) {
-    //     e.addChild(
-    //       new Graphics()
-    //         .circle(c.shape.x, c.shape.y, c.shape.r)
-    //         .stroke({ width: 1, color: 0x00ffff }),
-    //     )
-    //   } else {
-    //     e.addChild(new Graphics().poly(c.shape.vertices).stroke({ width: 1, color: 0x00ffff }))
-    //   }
-    // }
+    const ecs = world.getEntitiesWithComponent(Collider)
+    for (const { e, c } of ecs) {
+      e.addChild(c.shape.createDebugGraphics())
+    }
   }
 
   update(world: World, sb: SignalBus, dt: number): void {
     const ecs = world.getEntitiesWithComponent(Collider)
     for (let i = 0; i < ecs.length; i++) {
       const { e: eA, c: cA } = ecs[i]!
+
       for (let j = i + 1; j < ecs.length; j++) {
         const { e: eB, c: cB } = ecs[j]!
+
         cA.update(eA.position, eA.rotation)
         cB.update(eB.position, eB.rotation)
 
         if (cA.collides(cB)) {
-          sb.emit(new CollisionSignal(eA.id, eB.id))
+          // sb.emit(new CollisionSignal(eA.id, eB.id))
+          console.log('collide!')
         }
       }
-      // if (c.shape instanceof Rectangle) {
-      //   console.log('rect')
-      // } else if (c.shape instanceof Circle) {
-      //   console.log('circle')
-      // }
     }
   }
 }
